3212850c6d6f74415076d0f0d5879983cb5ffbe2
Leidy Mercedes <leidy.mercedes@gmail.com>
2018-04-18 19:53:11 +0200

    grafico listo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  private chartData: Array<any>;
+
+
+
+  ngOnInit() {
+
+    // Local Data //////////////////////////////////////////////////////////////////////
+    this.generateData();
+    // change the data periodically
+    //setInterval(() => this.generateData(), 3000);
+    // API Data ////////////////////////////////////////////////////////////////////////
+    //this._ApiService.getData().subscribe(result => { this.chartData = result })
+  }
+
+
+  generateData() {
+    this.chartData = [];
+    for (let i = 0; i < (8 + Math.floor(Math.random() * 10)); i++) {
+      this.chartData.push([
+        `Index ${i}`,
+        Math.floor(Math.random() * 100)
+      ]);
+    }
+  }
+
 }
